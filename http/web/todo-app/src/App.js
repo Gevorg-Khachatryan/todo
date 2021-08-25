937544b907bf554ca4ac6bd71600e76bac6b1340
Gevorg Khachatryan <gevorg.khachatryan@oneplanetstudios.com>
2021-08-25 18:56:14 +0400

    react app v1, commit 5

--- http/web/todo-app/src/App.js
+++ http/web/todo-app/src/App.js
@@ -10,22 +10,60 @@ class MyComponent extends React.Component {
         this.state = {
             error: null,
             isLoaded: false,
-            items: []
+            items: [],
+            data: []
         };
     }
 
     componentDidMount() {
-        this.homePage()
-        let actions = document.getElementsByClassName('actions')
-        ReactDOM.render([<button id="show_add_todo" className="button"
-                                 onClick={this.showAddTodo.bind(this)}>Add</button>,
-            <button id="show_todo_list" className="button"
-                    onClick={this.homePage.bind(this)}>List</button>], actions[0])
+        let x = this.homePage()
 
+        console.log(this.state.data, x)
     }
 
     render() {
-        return true;
+        const data = this.state.data
+        return <>
+            <div className="header">
+                <div className="title">TODO</div>
+            </div>
+
+            <div className="flex-container">
+                <div className="actions">
+                    <button id="show_add_todo" className="button"
+                            onClick={this.showAddTodo.bind(this)}>Add
+                    </button>
+                    <button id="show_todo_list" className="button"
+                            onClick={this.homePage.bind(this)}>List
+                    </button>
+                </div>
+
+                <div id="content">
+                    <table>
+                        <tbody>
+                        {data.map((item, index) => (
+                            <tr key={index}
+                                data-id={item.id}
+                                data-title={item.title}
+                                data-desc={item.desc}
+                                onClick={this.showUpdateTodo.bind(this)}>
+                                <td>{item.title}</td>
+                                <td>{item.desc}</td>
+                                <td><select data-id={item.id} onChange={this.changeTodoStatus.bind(this)}
+                                            value={item.completed ? 'complete' : 'uncomplete'}>
+                                    <option value="complete" className="complete">Completed
+                                    </option>
+                                    <option value="uncomplete" className="uncomplete">Uncompleted
+                                    </option>
+                                    <option value="delete" className="delete">Delete</option>
+                                </select></td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
     }
 
     changeTodoStatus(e) {
@@ -56,7 +94,7 @@ class MyComponent extends React.Component {
                     query: ' mutation deleteTodo {deleteTodo(id: "' + id + '") {success}}',
                 })
             })
-            .then(this.homePage.bind(this))
+                .then(this.homePage.bind(this))
         }
     }
 
@@ -129,6 +167,7 @@ class MyComponent extends React.Component {
     }
 
     homePage(e) {
+
         fetch('http://localhost:5000/graphql', {
             method: 'POST',
             headers: {
@@ -139,7 +178,9 @@ class MyComponent extends React.Component {
         })
             .then(response => response.json())
             .then(data => {
-                this.setState({data: data})
+                // this.setState({data: data})
+                let rows = []
+
                 let success
                 if (data.listTodos) {
                     success = data.listTodos.success
@@ -149,33 +190,46 @@ class MyComponent extends React.Component {
                 let list_todos = document.getElementById('content')
                 if (success) {
                     let todos = data.listTodos.post
-                    let rows = []
-                    todos.forEach((element, index, array) => {
-                        let td = React.createElement('td', {children: element.title})
-                        let row = React.createElement('tr', {
-                                className: 'info',
-                                data_id: element.id,
-                                data_title: element.title,
-                                data_desc: element.desc,
-                                onClick: this.showUpdateTodo.bind(this)
-                            },
-                            <td>{element.title}</td>,
-                            <td>{element.desc}</td>,
-                            <td><select data-id={element.id} onChange={this.changeTodoStatus.bind(this)} value={element.completed ? 'complete' : 'uncomplete'}>
-                                <option value="complete" className="complete">Completed
-                                </option>
-                                <option value="uncomplete" className="uncomplete">Uncompleted
-                                </option>
-                                <option value="delete" className="delete">Delete</option>
-                            </select></td>
-                        )
-                        rows.push(row)
-                    })
-                    ReactDOM.render(<table id="list_todos">
-                        <tbody>{rows}</tbody>
-                    </table>, list_todos)
+                    this.setState({'data': todos})
+                    // todos.forEach((element, index, array) => {
+                    //     let row = React.createElement('tr', {
+                    //             className: 'info',
+                    //             data_id: element.id,
+                    //             data_title: element.title,
+                    //             data_desc: element.desc,
+                    //             onClick: this.showUpdateTodo.bind(this)
+                    //         },
+                    //         <td>{element.title}</td>,
+                    //         <td>{element.desc}</td>,
+                    //         <td><select data-id={element.id} onChange={this.changeTodoStatus.bind(this)}
+                    //                     value={element.completed ? 'complete' : 'uncomplete'}>
+                    //             <option value="complete" className="complete">Completed
+                    //             </option>
+                    //             <option value="uncomplete" className="uncomplete">Uncompleted
+                    //             </option>
+                    //             <option value="delete" className="delete">Delete</option>
+                    //         </select></td>
+                    //     )
+                    //     rows.push(row)
+                    // })
+                    // this.setState({data: rows})
+
+                    // ReactDOM.render(<table id="list_todos">
+                    //     <tbody>{rows}</tbody>
+                    // </table>, list_todos)
                 }
             });
+        // return (
+        //
+        //     <table>
+        //         <tbody>
+        //         <tr>sada</tr>
+        //         {this.state.data.map((item, index) => (
+        //             {item}
+        //         ))}
+        //         </tbody>
+        //     </table>
+        // )
     }
 }
 
